a43cb913035d60bc20155c0aa562dcf21066f236
VoRtEx19 <lasvetlichnyy@edu.hse.ru>
2024-04-11 17:20:50 +0300

    Friends Initial
    - cut priofile pic to resolution 1:1
    - fixed urls
    - added buttons to friends list

--- src/pages/friends/Friends.jsx
+++ src/pages/friends/Friends.jsx
@@ -1,51 +1,74 @@
-import React, {useState} from 'react';
-import {Tab} from '@headlessui/react';
+import React, { useState } from 'react';
+import { Tab } from '@headlessui/react';
 import "./Friends.css"
+import profile from "./../../assets/profile.png"
 
 const Friends = () => {
     function classNames(...classes) {
         return classes.filter(Boolean).join(' ')
     }
 
-    let [categories] = useState({
-        Friends: [{
-            id: 1, title: 'Friend1'
-        }, {
-            id: 2, title: "Friend2"
-        },], Recent: [{
-            id: 1, title: 'User1'
-        }, {
-            id: 2,
-            title: 'User2',
-        },]
-    })
+    // getUserProfile().friends.map((friend, idx) => {id: idx, nickname: friend.nickname, ...})
+
+    let [friends] = useState([{
+        id: 1, nickName: 'Friend1', img: profile
+    }, {
+        id: 2, nickName: "Friend2", img: profile
+    },])
+
+    let [recents] = useState([{
+        id: 1, nickName: 'User1', img: profile
+    }, {
+        id: 2, nickName: 'User2', img: profile
+    },])
 
     return (<div className="friends-container">
         <Tab.Group>
             <Tab.List className="tab-list">
-                {Object.keys(categories).map((category) => (<Tab
-                    key={category}
-                    className={({selected}) => classNames('w-full rounded-lg py-2.5 text-sm font-medium leading-5', 'ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2', selected ? 'bg-white text-blue-700 shadow' : 'text-gray-500 hover:bg-white/[0.12] hover:text-white')}>
-                    {category}
-                </Tab>))}
+                <Tab
+                    key="Friends"
+                    className={({ selected }) => classNames('w-full rounded-lg py-2.5 text-sm font-medium leading-5', 'ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2', selected ? 'bg-white text-blue-700 shadow' : 'text-gray-500 hover:bg-white/[0.12] hover:text-white')}>
+                    Friends
+                </Tab>
+                <Tab
+                    key="Recents"
+                    className={({ selected }) => classNames('w-full rounded-lg py-2.5 text-sm font-medium leading-5', 'ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2', selected ? 'bg-white text-blue-700 shadow' : 'text-gray-500 hover:bg-white/[0.12] hover:text-white')}>
+                    Recents
+                </Tab>
             </Tab.List>
             <Tab.Panels className="tab-panels">
-                {Object.values(categories).map((people, idx) => (<Tab.Panel
+                {[friends, recents].map((people, idx) => <Tab.Panel
                     key={idx}
                     className={classNames('rounded-xl bg-white p-3', 'ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2')}
                 >
                     <ul>
                         {people.map((person) => (<li
                             key={person.id}
-                            className="flex relative rounded-md p-3 hover:bg-gray-100"
+                            className="w-full flex justify-between flex-row relative rounded-md p-3 border mb-1 hover:bg-gray-100"
                         >
-                            <img alt="" width={20} height={20} className="rounded-xl overflow-hidden"/>
-                            <h3 className="ml-1 text-sm font-medium leading-5">
-                                {person.title}
-                            </h3>
+                            <div className="flex">
+                                <img alt="" src={person.img} width={20} height={20} className="rounded-xl overflow-hidden" />
+                                <h3 className="ml-4 text-l font-medium leading-5">
+                                    {person.nickName}
+                                </h3>
+                            </div>
+                            <div className='flex'>
+                                {
+                                    idx ? <button className="ml-4 text-l font-medium leading-5">
+                                        Add Friend
+                                    </button> : <button className="ml-4 text-l font-medium leading-5">
+                                        Remove Friend
+                                    </button>
+                                }
+                                {
+                                    (idx > 0) && <button className="ml-4 text-l font-medium leading-5">
+                                        Report
+                                    </button>
+                                }
+                            </div>
                         </li>))}
                     </ul>
-                </Tab.Panel>))}
+                </Tab.Panel>)}
             </Tab.Panels>
         </Tab.Group>
     </div>)
--- src/static/Urls.js
+++ src/static/Urls.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:8009";
+export const BASE_URL = "http://92.63.107.124:8081";
 export const CONNECTION_URL = `${BASE_URL}/api/chat`;
 export const REFRESH_TOKEN_URL = `${BASE_URL}/auth/refresh-token`
