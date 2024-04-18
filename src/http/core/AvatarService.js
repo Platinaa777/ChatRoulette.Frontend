743b51fe5ea9cc0f1806d3af68bbc44c3a2a218f
VoRtEx19 <lasvetlichnyy@edu.hse.ru>
2024-04-19 00:33:23 +0300

    Avatar Bugfix
    Now avatars are sent properly, but link from server doesn't work

--- src/http/core/AvatarService.js
+++ src/http/core/AvatarService.js
@@ -2,14 +2,6 @@ import api from "../api";
 
 export class AvatarService {
     static async changeAvatar(picture) {
-
-        // console.log('HELLOOOOOO')
-        // console.log(picture)
-        const formFile = new FormData();
-        formFile.append('formFile', picture);
-
-        console.log(formFile.file)
-
-        return await api.post("avatar/change-avatar", {File: formFile}, {withCredentials: true});
+        return await api.post("avatar/change-avatar", {Picture: picture}, {withCredentials: true});
     }
 }
